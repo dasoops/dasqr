0931b013a26560a3d60799877667a6287298b0b5
Dasoops <DasoopsNicole@gmail.com>
2022-10-13 11:43:26 +0800

    fix(037): 🐛 reboot调试

--- core/src/main/java/com/dasoops/dasq/core/common/event/RebootEvent.java
+++ core/src/main/java/com/dasoops/dasq/core/common/event/RebootEvent.java
@@ -28,7 +28,9 @@ public class RebootEvent extends Thread {
     @Override
     public void run() {
         try {
+            log.info("1");
             Process process = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", dasqProperties.getRebootShellPath()});
+            log.info("2");
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line = "";
             while ((line = reader.readLine()) != null) {
