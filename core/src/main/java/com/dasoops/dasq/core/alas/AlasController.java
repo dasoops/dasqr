0322bb3ce0be7c1131d6f1fbe25a46e86e9519ef
Dasoops <DasoopsNicole@gmail.com>
2022-10-13 16:22:58 +0800

    feat(049): ✨ 添加了alas模块报警

---
+++ core/src/main/java/com/dasoops/dasq/core/alas/AlasController.java
@@ -0,0 +1,33 @@
+package com.dasoops.dasq.core.alas;
+
+import com.dasoops.dasq.core.common.entity.DasqProperties;
+import com.dasoops.dasq.core.common.util.KeywordUtil;
+import com.dasoops.dasq.core.cq.service.CqService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Title: AlasController
+ * @ClassPath com.dasoops.dasq.core.alas.AlasController
+ * @Author DasoopsNicole@Gmail.com
+ * @Date 2022/10/13
+ * @Version 1.0.0
+ * @Description: 唉控制器
+ */
+@RestController
+@RequestMapping("/alas")
+public class AlasController {
+
+    @Resource
+    private CqService cqService;
+    @Resource
+    private DasqProperties dasqProperties;
+
+    @GetMapping("/error")
+    public void alasError() {
+        cqService.sendMsg(true, Long.valueOf(dasqProperties.getDevGroupId()), KeywordUtil.buildAtCqCode(dasqProperties.getAdminId()) + "Alas模块发生异常,请求人类接管");
+    }
+}
