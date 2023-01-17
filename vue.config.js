17c28ce42dc08667b02012aaf0375968126a103b
dasoops <66471149+dasoops@users.noreply.github.com>
2023-01-17 16:04:23 +0800

    build() :修改打包文件目录

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: './',
+  outputDir:  '/var/lib/docker/volumes/nginx_data/_data/', //build之后静态文件输出路径
 })
