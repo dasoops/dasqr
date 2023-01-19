0a6fb75067db1d623229aa6bf1d2a2aa3b07abdc
dasoops <66471149+dasoops@users.noreply.github.com>
2023-01-19 16:02:18 +0800

    ci(): rm vue.config.js

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  publicPath: './',
-  outputDir:  '/var/lib/docker/volumes/nginx_data/_data/', //build之后静态文件输出路径
-})
