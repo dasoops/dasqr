ad54876ba2bcf56b272f0463197fec11d8b97d53
Dasoops <DasoopsNicole@gmail.com>
2023-05-10 16:18:54 +0800

    feat: 添加reply插件demo

--- plugin/plugin-loaj/plugin-reply/src/main/kotlin/com/dasoops/dasqr/plugin/reply/ReplyListenerHost.kt
+++ plugin/plugin-loaj/plugin-reply/src/main/kotlin/com/dasoops/dasqr/plugin/reply/ReplyListenerHost.kt
@@ -3,8 +3,14 @@ package com.dasoops.dasqr.plugin.reply
 import cn.hutool.cache.CacheUtil
 import cn.hutool.cache.impl.TimedCache
 import com.dasoops.dasqr.core.DasqrListenerHost
-import net.mamoe.mirai.event.EventHandler
+import com.dasoops.dasqr.core.IBot
+import com.dasoops.dasqr.core.IBot.bot
+import net.mamoe.mirai.event.*
 import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.at
+import net.mamoe.mirai.message.data.firstIsInstanceOrNull
+import net.mamoe.mirai.message.nextMessage
 import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 
@@ -22,7 +28,10 @@ open class ReplyListenerHost : DasqrListenerHost() {
 
     @EventHandler
     open suspend fun handle(event: GroupMessageEvent) {
-        event.intercept()
+        if ((event.message.firstIsInstanceOrNull<At>()?.target == bot.id)) {
+            event.subject.sendMessage("at我干啥子嘞")
+            event.intercept()
+        }
     }
 
     fun getData(): Collection<Reply> {
