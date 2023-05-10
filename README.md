8acb672dc4794f37a3a86d9d1ef96bf4913ffb7d
Dasoops <DasoopsNicole@gmail.com>
2023-05-10 12:24:35 +0800

    doc: 修改README.md

--- README.md
+++ README.md
@@ -4,12 +4,21 @@
 [Mirai](https://gitter.im/mamoe/mirai?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
 的qqBot实现,开发中
 
-branch v2-server 是老板,基于go_cqhttp,已经挺完善的
+依赖仓库 [common](https://github.com/dasoops/common)
 
-但是有部分代码越堆越屎+发现了go_cqhttp基于mirai,而mirai是基于jvm的,所以..
+task `launchTest` : 打包后在工作路径测试,用于测试插件加载等(mcl抄来的)
 
-重构(第三次)
+在项目根目录下新建`config.gradle`并添加以下代码可重写工作目录
+```kotlin script
+ext {
+    dasqrWorkingDir = "D:/temp/dasqr"
+}
+```
 
-branch v2-web 老板的前端,一边学vue一边写的,屎的很
+`plugin-system`     提供系统级插件,包括数据库加载配置插件等
 
-branch v1 屎山
+`plugin-loaj`       乱七八糟的插件
+
+`plugin-test`       开发环境可直接在该项目下添加其他插件的依赖
+                    运行com.dasoops.dasqr.plugin.MainKt.main()进行测试
+ 
--- plugin/plugin-system/plugin-system-register/src/main/kotlin/com/dasoops/dasqr/plugin/auth/AuthPluginPool.kt
+++ plugin/plugin-system/plugin-system-register/src/main/kotlin/com/dasoops/dasqr/plugin/auth/AuthPluginPool.kt
@@ -8,8 +8,16 @@ import com.dasoops.dasqr.core.DasqrListenerHost
 import com.dasoops.dasqr.core.IBot
 import com.dasoops.dasqr.core.config.PluginConfig
 import com.dasoops.dasqr.core.plugin.PluginPool
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
+import net.mamoe.mirai.event.*
 import org.slf4j.LoggerFactory
+import org.springframework.util.ClassUtils
 import java.lang.reflect.Method
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.reflect.full.functions
 
 /**
  * 身份验证插件池
@@ -22,11 +30,12 @@ object AuthPluginPool : PluginPool {
     val loadList = mutableSetOf<DasqrListenerHost>()
 
     override fun init(pluginConfig: PluginConfig) {
+        log.info("[plugin-system-auth]use AuthPluginPool")
         val scanPathList = pluginConfig.scanPath
         Resources.scan(*scanPathList.toTypedArray()).filter {
             DasqrListenerHost::class.java.isAssignableFrom(it)
         }.forEach {
-            IBot.eventChannel.registerListenerHost(proxy(it.kotlin.objectInstance as DasqrListenerHost))
+            IBot.eventChannel.registerListenerHost0(proxy(it.kotlin.objectInstance as DasqrListenerHost))
             log.info("load listener host: ${it.name}")
         }
     }
@@ -37,5 +46,59 @@ object AuthPluginPool : PluginPool {
                 return RandomUtil.randomBoolean()
             }
         })
+    }
+
+    fun EventChannel<*>.registerListenerHost0(
+        host: ListenerHost,
+        coroutineContext: CoroutineContext = EmptyCoroutineContext,
+    ) {
+        val jobOfListenerHost: Job?
+        val coroutineContext0 = if (host is SimpleListenerHost) {
+            val listenerCoroutineContext = host.coroutineContext
+            val listenerJob = listenerCoroutineContext[Job]
+
+            val rsp = listenerCoroutineContext.minusKey(Job) +
+                    coroutineContext +
+                    (listenerCoroutineContext[CoroutineExceptionHandler] ?: EmptyCoroutineContext)
+
+            val registerCancelHook = when {
+                listenerJob === null -> false
+
+                // Registering cancellation hook is needless
+                // if [Job] of [EventChannel] is same as [Job] of [SimpleListenerHost]
+                (rsp[Job] ?: this.defaultCoroutineContext[Job]) === listenerJob -> false
+
+                else -> true
+            }
+
+            jobOfListenerHost = if (registerCancelHook) {
+                listenerCoroutineContext[Job]
+            } else {
+                null
+            }
+            rsp
+        } else {
+            jobOfListenerHost = null
+            coroutineContext
+        }
+        for (method in ClassUtils.getUserClass(host).declaredMethods) {
+            method.getAnnotation(EventHandler::class.java)?.let { it ->
+                val listener = Method::class.functions.find { func ->
+                    func.name == "registerEventHandler"
+                }?.call(host, this, it, coroutineContext0) as Listener<Event>
+
+//                val listener = method.registerEventHandler(host, this, it, coroutineContext0)
+                // For [SimpleListenerHost.cancelAll]
+                jobOfListenerHost?.invokeOnCompletion { exception ->
+                    listener.cancel(
+                        when (exception) {
+                            is CancellationException -> exception
+                            is Throwable -> CancellationException(null, exception)
+                            else -> null
+                        }
+                    )
+                }
+            }
+        }
     }
 }
